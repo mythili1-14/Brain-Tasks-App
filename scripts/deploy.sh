39768a70e6fe1b1b5910049eb43726dadbfeffd7
mythili1-14 <mythili14k@gmail.com>
2025-09-24 14:58:24 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Apply Kubernetes deployment and service
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+kubectl apply -f kubernetes/deployment.yaml
+kubectl apply -f kubernetes/service.yaml
 
 # Wait for the deployment to be ready
 kubectl rollout status deployment/brain-tasks-deployment --namespace default
